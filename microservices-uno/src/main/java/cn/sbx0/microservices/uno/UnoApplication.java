92b4b5a14bf9f65c2d1b020ee41b93ff533659d7
sbx0 <782715416@qq.com>
2022-03-23 16:17:51 +0800

    add RedisTemplateConfig

--- microservices-uno/src/main/java/cn/sbx0/microservices/uno/UnoApplication.java
+++ microservices-uno/src/main/java/cn/sbx0/microservices/uno/UnoApplication.java
@@ -1,17 +1,11 @@
 package cn.sbx0.microservices.uno;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
@@ -23,18 +17,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class UnoApplication {
     public static void main(String[] args) {
         SpringApplication.run(UnoApplication.class, args);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisStringTemplate(LettuceConnectionFactory lettuceConnectionFactory, ObjectMapper objectMapper) {
-        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
-        // value serializer
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        // hash value serializer
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return redisTemplate;
     }
 }
