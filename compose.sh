ed33f58faa61ee63ba1f8372da4bf400cc5eea81
sbx0 <782715416@qq.com>
2022-03-25 11:20:44 +0800

    compose sh all in one

--- compose.sh
+++ compose.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 # chmod u+x compose.sh
-docker-compose -f docker-compose-prod.yml --env-file .env up -d microservices-configuration
+
+build() {
+  docker-compose -f docker-compose-prod.yml --env-file .env build
+}
+
+up() {
+  docker-compose -f docker-compose-prod.yml --env-file .env up -d microservices-configuration
+}
+
+build-and-up() {
+  build
+  up
+}
+
+log() {
+  docker-compose logs -f --tail=1
+}
+
+case "$1" in
+"build" | "b")
+  build
+  ;;
+"up" | "p")
+  up
+  ;;
+"build-and-up" | "bp")
+  build-and-up
+  ;;
+"log" | "l")
+  log
+  ;;
+*)
+ up
+  ;;
+esac
