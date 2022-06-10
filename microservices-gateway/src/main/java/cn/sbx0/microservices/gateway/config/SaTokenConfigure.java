1e2f5c43893148adf091f9a9981e9cd633be9309
sbx0 <782715416@qq.com>
2022-06-10 10:52:38 +0800

    add permission verification

--- microservices-gateway/src/main/java/cn/sbx0/microservices/gateway/config/SaTokenConfigure.java
+++ microservices-gateway/src/main/java/cn/sbx0/microservices/gateway/config/SaTokenConfigure.java
@@ -26,10 +26,13 @@ public class SaTokenConfigure {
                 // 拦截地址
                 .addInclude("/ACCOUNT/**")
                 .addInclude("/UNO/**")
+                .addInclude("/HOME/**")
                 // 开放地址
                 .addExclude("/favicon.ico")
                 .addExclude("/actuator/*")
                 .addExclude("/UNO/uno/room/list")
+                .addExclude("/HOME/community/table/structure")
+                .addExclude("/HOME/communities")
                 // 鉴权方法：每次访问进入
                 .setAuth(obj -> {
                     // 登录校验 -- 拦截所有路由，并排除/user/doLogin 用于开放登录
