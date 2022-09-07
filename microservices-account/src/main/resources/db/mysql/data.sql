f514a4e33fe1842be8fee23d6270a0e4fc1ff003
sbx0 <782715416@qq.com>
2022-09-07 13:58:44 +0800

    decoupling

--- microservices-account/src/main/resources/db/mysql/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO account (id, username, password, nickname, email, del_flag, enable_flag, remark)
-VALUES (1, 'sbx0', '$2a$10$.VfoB8BM56FqqVbKjHa3jeZbFvZLqwaZFlt5Ahj32zpOm55z3ubza', 'test', 'test', 0, 1, 'test');
